ad15038a1b9857e7cf6164212bd5a5f0c57149bf
onojasunday <47522696+onojasunday@users.noreply.github.com>
2021-03-10 14:46:55 +0100

    Add files via upload

---
+++ PRogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor=ConsoleColor.Blue; 
+            String MyName = "onoja";
+            String Message = "My name is " + MyName;
+            String CapsMessage = Message.ToUpper();
+            Console.WriteLine(MyName);
+            Console.WriteLine(Message);
+            Console.WriteLine(CapsMessage);
+            Console.Read();
+
+        }
+    }
+}
