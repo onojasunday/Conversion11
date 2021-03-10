02ba23b107e4278336c00d5562e7b54265c7fd0e
onojasunday <47522696+onojasunday@users.noreply.github.com>
2021-03-10 14:32:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            double mydouble = 13.37;
+            //explicit conversion
+            //int myInt;
+           // myInt = (int)mydouble;
+            //String MyString = mydouble.ToString();
+            String MyString = "12";
+            String mySecondString = "23";
+            String result = MyString + mySecondString;
+            Console.WriteLine(result);
+
+
+            int num1 = Int32.Parse(MyString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+
+            Console.WriteLine(resultInt);
+
+
+
+
+           // Console.WriteLine(myInt);
+           // Console.WriteLine(MyString);
+            Console.Read();
+
+        
+           
+        }
+    }
+}
